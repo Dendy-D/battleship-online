8b8ca859566257738a43721f89badf77e41b3fe2
daniil_vasilenko <daniilvasilenko2@mail.ru>
2023-03-26 00:54:34 +0300

    feat: create basic structure

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:4000');
+
+const App: React.FC = () => {
+  return (
+    <div>
+      gg
+    </div>
+  );
+};
+
+export default App;
