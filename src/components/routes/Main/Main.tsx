a65246ed2b54c224c048e897c5c681eea2941412
Dendy-D <daniilvasilenko2@mail.ru>
2023-04-05 22:14:10 +0300

    feat(routes): add Menu plug

--- src/components/routes/Main/Main.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Main: React.FC = () => (
-  <div>Main</div>
-);
-
-export default Main;
---
+++ src/components/routes/Menu/Menu.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './Menu.module.scss';
+
+const Menu = () => (
+  <div className={classes.menu}>
+    Menu
+  </div>
+);
+
+export default Menu;
