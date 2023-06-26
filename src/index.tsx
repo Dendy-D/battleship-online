36c8f6ab427293bd1d63eef558e2a64c0ea8941b
Dendy-D <daniilvasilenko2@mail.ru>
2023-06-27 02:44:26 +0300

    feat: install react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
@@ -11,9 +12,10 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-
 );
 
 reportWebVitals();
