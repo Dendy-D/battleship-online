05ed10d073820b484006aaaef4a92908d1ac4c79
Dendy-D <daniilvasilenko2@mail.ru>
2023-06-28 05:46:51 +0300

    feat: edit Button component styles and add route props

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import classes from './Button.module.scss';
 
 type Props = {
   children: React.ReactNode;
+  route?: string;
 };
 
-const Button: React.FC<Props> = ({ children }) => <div className={classes.component}>{children}</div>;
+const Button: React.FC<Props> = ({ children, route }) => {
+  if (route) {
+    return (
+      <Link to={route} className={classes.component}><span>{children}</span></Link>
+    );
+  }
+  return <div className={classes.component}>{children}</div>;
+};
 
 export default Button;
